c94f956cabb55a80f231e47725ce91a813dd614b
Danae Martínez <martinezvd@dms.umontreal.ca>
2020-09-24 18:11:31 -0400

    add the mutate verb

--- IntroTidyverse.R
+++ IntroTidyverse.R
@@ -34,9 +34,25 @@ gapminder %>%
   filter(year=='1957') %>%
   arrange(desc(pop))
 
+# Use mutate to change lifeExp to be in months
+gapminder %>%
+  mutate(lifeExp = lifeExp*12)
+
+# Use mutate to create a new column called lifeExpMonths
+gapminder %>% 
+  mutate(lifeExpMonths = lifeExp*12)
+
+# Filter, mutate, and arrange the gapminder dataset
+gapminder %>% 
+  filter(year=='2007') %>%  
+  mutate(lifeExpMonths = 12 * lifeExp) %>%  
+  arrange(desc(lifeExpMonths))
+
 #----------------------------#
 #---# Data visualization #---#
 #----------------------------#
+
+
 
 #----------------------------------#
 #---# Grouping and summarizing #---#
