a5f6211e0a53c68d6f6247ac5665af3d6ea36e72
Danae Martínez <martinezvd@dms.umontreal.ca>
2020-09-24 18:33:23 -0400

    adding the summarize verb

--- IntroTidyverse.R
+++ IntroTidyverse.R
@@ -110,6 +110,23 @@ gapminder %>%
 #---# Grouping and summarizing #---#
 #----------------------------------#
 
+# Summarize to find the median life expectancy
+
+gapminder %>% 
+  summarise(medianLifeExp = median(lifeExp))
+
+# Filter for 1957 then summarize the median life expectancy
+gapminder %>% 
+  filter(year==1957) %>% 
+  summarize(medianLifeExp = median(lifeExp)) 
+
+# Filter for 1957 then summarize the median life expectancy and the maximum GDP per capita
+gapminder %>% 
+  filter(year==1957) %>% 
+  summarize(medianLifeExp= median(lifeExp),
+            maxGdpPercap = max(gdpPercap)) 
+
+
 #---------------------------------#
 #---# Types of visualizations #---#
 #---------------------------------#
