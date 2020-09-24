60f5c17536438c7fb25a7149b066419246dd60d6
Danae Martínez <martinezvd@dms.umontreal.ca>
2020-09-24 18:06:29 -0400

    add the filter example

--- IntroTidyverse.R
+++ IntroTidyverse.R
@@ -1,15 +1,36 @@
 
-# Introduction to the Tidyverse
+#---------------------------------------#
+#---# Introduction to the Tidyverse #---#
+#---------------------------------------#
 
 library(gapminder)
+library(dplyr)
+
 str(gapminder)
+gapminder # Look at the gapminder dataset
 
-# Data wrangling
+#------------------------#
+#---# Data wrangling #---#
+#------------------------#
 
-# Data visualization
+# Filter the gapminder dataset for the year 1957
+gapminder %>% 
+  filter(year==1957)
 
-# Grouping and summarizing
+# Filter for China in 2002
+gapminder %>% 
+  filter(year=='2002'& country=='China')
 
-# Types of visualizations
 
+#----------------------------#
+#---# Data visualization #---#
+#----------------------------#
+
+#----------------------------------#
+#---# Grouping and summarizing #---#
+#----------------------------------#
+
+#---------------------------------#
+#---# Types of visualizations #---#
+#---------------------------------#
 
